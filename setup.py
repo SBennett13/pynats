5d4873ee114ffb5fcf38000dc6188585a76c9c19
Scott Bennett <scottbennett027@gmail.com>
2024-10-13 21:41:11 -0400

    prepare for release

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(
     version="0.0.1",
     packages=["pynats", "pynats.protocol"],
     zip_safe=True,
+    long_description="A synchronous NATS client, because not everyone's codebase is built for asyncio",
 )
